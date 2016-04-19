cbc7604cb3d92c1e2627563a876a278789f645ad
tinceetlintallo <tinceetlintallo@yahoo.com>
2016-04-19 14:29:23 +0700

    Added files via upload

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.jgen = new System.Windows.Forms.TextBox();
+            this.jpop = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.ProgressBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.iterasi = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Batas atas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Batas bawah";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.y2);
+            this.groupBox1.Controls.Add(this.y1);
+            this.groupBox1.Controls.Add(this.x2);
+            this.groupBox1.Controls.Add(this.x1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(149, 85);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(85, 56);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(49, 20);
+            this.y2.TabIndex = 3;
+            this.y2.Text = "0";
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(14, 56);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(49, 20);
+            this.y1.TabIndex = 2;
+            this.y1.Text = "7.3";
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(85, 20);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(49, 20);
+            this.x2.TabIndex = 1;
+            this.x2.Text = "-5.0";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(14, 19);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(49, 20);
+            this.x1.TabIndex = 0;
+            this.x1.Text = "9.8";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.iterasi);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.jgen);
+            this.groupBox2.Controls.Add(this.jpop);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(186, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 172);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // jgen
+            // 
+            this.jgen.Location = new System.Drawing.Point(112, 52);
+            this.jgen.Name = "jgen";
+            this.jgen.Size = new System.Drawing.Size(62, 20);
+            this.jgen.TabIndex = 6;
+            this.jgen.Text = "1000";
+            // 
+            // jpop
+            // 
+            this.jpop.Location = new System.Drawing.Point(112, 23);
+            this.jpop.Name = "jpop";
+            this.jpop.Size = new System.Drawing.Size(62, 20);
+            this.jpop.TabIndex = 5;
+            this.jpop.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Jumlah Generasi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Jumlah Populasi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Proses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(16, 218);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(373, 23);
+            this.pb1.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(16, 260);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(491, 185);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Pop";
+            this.columnHeader1.Width = 35;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "x1";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "x2";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "sigma1";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "sigma2";
+            this.columnHeader5.Width = 90;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Fitness";
+            // 
+            // iterasi
+            // 
+            this.iterasi.Location = new System.Drawing.Point(110, 77);
+            this.iterasi.Name = "iterasi";
+            this.iterasi.Size = new System.Drawing.Size(64, 20);
+            this.iterasi.TabIndex = 17;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(10, 80);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(94, 13);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "Generasi Terakhir ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 459);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox jgen;
+        private System.Windows.Forms.TextBox jpop;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar pb1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.TextBox iterasi;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        int my, mx, k;
+        Graphics g;
+        long jp, jg, cg;
+        double pc, pm, th, xa, xb, ya, yb, pk;
+        ListViewItem lv;
+        double [] pop = new double[1];
+        double[] pop1 = new double[1];
+        double[] pop2 = new double[1];
+        double[] pop3 = new double[1];
+        double[] pop4 = new double[1];
+        double[] pop5 = new double[1];
+        Random r = new Random();
+        int[][] clus = new int[1][];
+        
+        public Form1()
+        {
+            InitializeComponent();
+                            
+        }
+
+        int ex(double x) {
+            return (int)Math.Floor((xa + x) * 350 / (xa - xb));
+        }
+
+        int ey(double y) {
+            return (int)Math.Floor((ya + y) * 350 / (ya - yb));
+        }
+
+        double fitkrom(double x, double y)
+        {
+            return Math.Round(19+x*Math.Sin(x*Math.PI)+(10-y)*Math.Sin(y*Math.PI),k);
+        }
+                
+        double N() { 
+            double r1,r2;
+            r1 = r.NextDouble();
+            r2 = r.NextDouble();
+            return Math.Round(Math.Sqrt(-2*Math.Log(r1))*Math.Sin(2*Math.PI*r2),k);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            k =5;
+            pb1.Minimum = 0;
+            jp = Convert.ToInt32(jpop.Text);
+            jg = Convert.ToInt32(jgen.Text);
+            pb1.Maximum = (int)jg +1;
+            pb1.Value = 0;
+            Array.Resize(ref pop,(int)jp);
+            Array.Resize(ref pop1,(int)jp);
+            Array.Resize(ref pop2, (int)jp);
+            Array.Resize(ref pop3, (int)jp);
+            Array.Resize(ref pop4, (int)jp*7);
+            Array.Resize(ref pop5, (int)jp*7);
+            
+            double[] fit = new double[jp];
+            double[] fit1= new double[jp*7];
+            xa = Double.Parse (x1.Text);
+            xb = Convert.ToDouble(x2.Text);
+            ya = Convert.ToDouble(y1.Text);
+            yb = Convert.ToDouble(y2.Text);
+            cg = -1;
+            
+            //populasi awal
+            
+            for (int j = 0; j < jp;j++ ){
+                //for (int i = 0; i < mx + my; i++) 
+                pop[j] = Math.Round(r.Next(Convert.ToInt32(xb * Math.Pow(10, k)), Convert.ToInt32(xa * Math.Pow(10, k)))/
+                    Math.Pow(10,k),k);
+                pop1[j] = Math.Round(r.Next(Convert.ToInt32(yb * Math.Pow(10, k)), Convert.ToInt32(ya * Math.Pow(10, k)))/
+                    Math.Pow(10,k),k);
+                pop2[j] = Math.Round(r.NextDouble(),k);
+                pop3[j] = Math.Round(r.NextDouble(),k);
+            }
+            
+            for (int i = 0; i < jp; i++){
+                fit[i] = fitkrom(pop[i],pop1[i]);
+            }
+   
+            cg++;
+            Array.Sort(fit);
+            while ((cg<=jg) && (pk<=th) ){
+                for (int i = 0; i < jp;i++)
+                {
+                    int z = 0;
+                    for (int j = 0; j < 7; j++)
+                    {
+                        pop4[i * 7 + j] = pop[i] + pop2[i] * N();
+                        pop5[i * 7 + j] = pop1[i] + pop3[i] * N();
+                        fit1[i * 7 + j] = fitkrom(pop4[i * 7 + j], pop5[i * 7 + j]);
+                        if (fit1[i * 7 + j] > fit[i])
+                        {
+                            z++;    
+                        } 
+
+                    }
+                    if (z >= 2)
+                    {
+                        pop2[i] = Math.Round(1.1 * pop2[i], k); ;
+                        pop3[i] = Math.Round(1.1 * pop3[i], k);
+                    }
+                    else {
+                        pop2[i] = Math.Round(0.9 * pop2[i], k);
+                        pop3[i] = Math.Round(0.9 * pop3[i], k);
+                    }
+                }
+                Array.Sort(fit1);
+
+                for (int i = 0; i < jp*7;i++)
+                {
+                   int j=Array.IndexOf(fit1,fitkrom(pop4[i],pop5[i])); 
+                   if (j>jp*6-1) {
+                       pop[jp * 7 - j-1] = pop4[i];
+                       pop1[jp * 7 - j-1] = pop5[i];
+                   }
+                }
+                cg++;
+                pb1.Value++;
+            }
+            
+            listView1.Columns.Add("Pop", 50, HorizontalAlignment.Left);
+            listView1.Columns.Add("X1", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("X2", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("sigma1", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("sigma2", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("Fitness", 90, HorizontalAlignment.Left);
+            iterasi.Text = Convert.ToString(cg-1);
+            for (int i = 1; i <= jp; i++)
+            {
+                lv = new ListViewItem(Convert.ToString(i)); 
+                lv.SubItems.Add(Convert.ToString(pop[i-1]));
+                lv.SubItems.Add(Convert.ToString(pop1[i-1]));
+                lv.SubItems.Add(Convert.ToString(pop2[i - 1]));
+                lv.SubItems.Add(Convert.ToString(pop3[i - 1]));
+                lv.SubItems.Add(Convert.ToString(fit[i-1]));
+                listView1.Items.Add(lv);
+            }
+            for (int i = 0; i < jp; i++)
+            {
+                //g.FillEllipse(new SolidBrush(Color.Blue), ex(Math.Round(Encodex(bintodes(pop[i].Substring(0, mx))), k)),
+                  //  ey(Math.Round(Encodey(bintodes(pop[i].Substring(mx, my))), k)), 7, 7);
+            }
+            pb1.Value = pb1.Maximum;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            iterasi.Text = "0";
+           
+        }
+
+        private void pnl_Draw_Paint(object sender, PaintEventArgs e)
+        {
+           
+            Pen p = new Pen(Color.Black,1);
+          
+        }
+    }
+}
